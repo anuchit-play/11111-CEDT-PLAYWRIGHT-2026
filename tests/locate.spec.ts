ffbcda51ff142c1d59dc5ec53c7ed340b77c083f
Anuchit Charoenkeskit <anuchit@playtorium.co.th>
2026-02-23 11:36:16 +0700

    change spec on pipeline

--- tests/locate.spec.ts
+++ tests/locate.spec.ts
@@ -17,6 +17,11 @@ test.describe('Locate elements', async () => {
 	})
 
 	test('Locate element using index', async ({ page }) => {
+		await page.goto('https://katalon-demo-cura.herokuapp.com/')
+		// locate element
+		await page
+			.getByRole('link', { name: 'Make Appointment', exact: true })
+			.click()
 		await page.getByPlaceholder('Username').nth(1).fill('username')
 		await page.getByPlaceholder('Username').last().fill('useername')
 		await page.getByPlaceholder('Username').first().fill('useername')
